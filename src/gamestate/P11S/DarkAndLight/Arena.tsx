aa5b5a7c813c9e62fdaab824bc7ec3ee130b7140
Jess Law <32jezza@gmail.com>
2023-11-20 01:06:22 +0000

    Add arrows back to tether

--- src/gamestate/P11S/DarkAndLight/Arena.tsx
+++ src/gamestate/P11S/DarkAndLight/Arena.tsx
@@ -7,7 +7,7 @@ import { Arena as P11SArena } from "../P11SArena";
 
 import { DarkAndLightPlayer, isTetherSafe } from "./gameState";
 import { DangerPuddle } from "../../Mechanics/DangerPuddles";
-import { Point } from "@flatten-js/core";
+import { Point, vector } from "@flatten-js/core";
 
 const Tether = (props: {
   player: DarkAndLightPlayer;
@@ -16,6 +16,34 @@ const Tether = (props: {
   const { player, tetheredTo } = props;
   const halfwayX = (player.position.x + tetheredTo.position.x) / 2;
   const halfwayY = (player.position.y + tetheredTo.position.y) / 2;
+
+  const angle = vector(
+    player.position,
+    player.position.translate(0, 1)
+  ).angleTo(vector(player.position, tetheredTo.position));
+
+  const source =
+    player.tetherLength === "Long"
+      ? player.position
+      : player.position.translate(0, 0.05).rotate(angle, player.position);
+  const angle2 = player.tetherLength === "Long" ? angle : Math.PI + angle;
+  const t1 = source.translate(0, 0.05).rotate(angle2 + 0.5, source);
+  const t2 = source.translate(0, 0.05).rotate(angle2 - 0.5, source);
+
+  const tetheredSource =
+    player.tetherLength === "Long"
+      ? tetheredTo.position
+      : tetheredTo.position
+          .translate(0, 0.05)
+          .rotate(Math.PI + angle, tetheredTo.position);
+  const s1 = tetheredSource
+    .translate(0, 0.05)
+    .rotate(Math.PI + angle2 + 0.5, tetheredSource);
+  const s2 = tetheredSource
+    .translate(0, 0.05)
+    .rotate(Math.PI + angle2 - 0.5, tetheredSource);
+
+  const safe = isTetherSafe(player, tetheredTo);
   return (
     <svg
       style={{
@@ -32,29 +60,67 @@ const Tether = (props: {
         y1={player.position.y}
         x2={halfwayX}
         y2={halfwayY}
-        strokeWidth={0.02}
-        stroke={
-          isTetherSafe(player, tetheredTo)
-            ? "blue"
-            : player.debuff === "Dark"
-            ? "purple"
-            : "yellow"
-        }
+        strokeWidth={0.01}
+        stroke={safe ? "blue" : player.debuff === "Dark" ? "purple" : "yellow"}
       />
+      {!safe && (
+        <>
+          <line
+            x1={source.x}
+            y1={source.y}
+            x2={t1.x}
+            y2={t1.y}
+            strokeWidth={0.01}
+            stroke={
+              safe ? "blue" : player.debuff === "Dark" ? "purple" : "yellow"
+            }
+          />
+          <line
+            x1={source.x}
+            y1={source.y}
+            x2={t2.x}
+            y2={t2.y}
+            strokeWidth={0.01}
+            stroke={
+              safe ? "blue" : player.debuff === "Dark" ? "purple" : "yellow"
+            }
+          />
+        </>
+      )}
       <line
         x1={tetheredTo.position.x}
         y1={tetheredTo.position.y}
         x2={halfwayX}
         y2={halfwayY}
-        strokeWidth={0.02}
+        strokeWidth={0.01}
         stroke={
-          isTetherSafe(player, tetheredTo)
-            ? "blue"
-            : tetheredTo.debuff === "Dark"
-            ? "purple"
-            : "yellow"
+          safe ? "blue" : tetheredTo.debuff === "Dark" ? "purple" : "yellow"
         }
       />
+      {!safe && (
+        <>
+          <line
+            x1={tetheredSource.x}
+            y1={tetheredSource.y}
+            x2={s1.x}
+            y2={s1.y}
+            strokeWidth={0.01}
+            stroke={
+              safe ? "blue" : tetheredTo.debuff === "Dark" ? "purple" : "yellow"
+            }
+          />
+          <line
+            x1={tetheredSource.x}
+            y1={tetheredSource.y}
+            x2={s2.x}
+            y2={s2.y}
+            strokeWidth={0.01}
+            stroke={
+              safe ? "blue" : tetheredTo.debuff === "Dark" ? "purple" : "yellow"
+            }
+          />
+        </>
+      )}
     </svg>
   );
 };
