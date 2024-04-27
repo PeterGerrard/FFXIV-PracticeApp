4e606de3816d0b3a0e7fb16f928d64508ec79cd4
Jess Law <32jezza@gmail.com>
2024-04-27 12:17:04 +0100

    remove bad positions

--- src/gamestate/P12SP2/Classical1/validDiePositions.tsx
+++ src/gamestate/P12SP2/Classical1/validDiePositions.tsx
@@ -1328,28 +1328,6 @@ export const validDiePositions: [
     },
     {
       pos: point(3, 1),
-      pyramidPos: point(3, 0),
-      squarePos: point(3, 2),
-    },
-  ],
-  [
-    {
-      pos: point(0, 1),
-      pyramidPos: point(1, 1),
-      squarePos: point(0, 0),
-    },
-    {
-      pos: point(1, 2),
-      pyramidPos: point(0, 2),
-      squarePos: point(2, 2),
-    },
-    {
-      pos: point(2, 0),
-      pyramidPos: point(2, 1),
-      squarePos: point(1, 0),
-    },
-    {
-      pos: point(3, 1),
       pyramidPos: point(3, 2),
       squarePos: point(3, 0),
     },
@@ -1369,28 +1347,6 @@ export const validDiePositions: [
       pos: point(2, 0),
       pyramidPos: point(1, 0),
       squarePos: point(2, 1),
-    },
-    {
-      pos: point(3, 1),
-      pyramidPos: point(3, 0),
-      squarePos: point(3, 2),
-    },
-  ],
-  [
-    {
-      pos: point(0, 1),
-      pyramidPos: point(1, 1),
-      squarePos: point(0, 0),
-    },
-    {
-      pos: point(1, 2),
-      pyramidPos: point(2, 2),
-      squarePos: point(0, 2),
-    },
-    {
-      pos: point(2, 0),
-      pyramidPos: point(2, 1),
-      squarePos: point(1, 0),
     },
     {
       pos: point(3, 1),
@@ -1709,72 +1665,6 @@ export const validDiePositions: [
   [
     {
       pos: point(0, 1),
-      pyramidPos: point(0, 0),
-      squarePos: point(0, 2),
-    },
-    {
-      pos: point(1, 0),
-      pyramidPos: point(1, 1),
-      squarePos: point(2, 0),
-    },
-    {
-      pos: point(2, 2),
-      pyramidPos: point(2, 1),
-      squarePos: point(1, 2),
-    },
-    {
-      pos: point(3, 1),
-      pyramidPos: point(3, 0),
-      squarePos: point(3, 2),
-    },
-  ],
-  [
-    {
-      pos: point(0, 1),
-      pyramidPos: point(0, 0),
-      squarePos: point(0, 2),
-    },
-    {
-      pos: point(1, 0),
-      pyramidPos: point(1, 1),
-      squarePos: point(2, 0),
-    },
-    {
-      pos: point(2, 2),
-      pyramidPos: point(2, 1),
-      squarePos: point(1, 2),
-    },
-    {
-      pos: point(3, 1),
-      pyramidPos: point(3, 2),
-      squarePos: point(3, 0),
-    },
-  ],
-  [
-    {
-      pos: point(0, 1),
-      pyramidPos: point(0, 0),
-      squarePos: point(0, 2),
-    },
-    {
-      pos: point(1, 0),
-      pyramidPos: point(1, 1),
-      squarePos: point(2, 0),
-    },
-    {
-      pos: point(2, 2),
-      pyramidPos: point(1, 2),
-      squarePos: point(2, 1),
-    },
-    {
-      pos: point(3, 1),
-      pyramidPos: point(3, 2),
-      squarePos: point(3, 0),
-    },
-  ],
-  [
-    {
-      pos: point(0, 1),
       pyramidPos: point(0, 2),
       squarePos: point(0, 0),
     },
@@ -1902,94 +1792,6 @@ export const validDiePositions: [
       pos: point(3, 1),
       pyramidPos: point(2, 1),
       squarePos: point(3, 0),
-    },
-  ],
-  [
-    {
-      pos: point(0, 1),
-      pyramidPos: point(0, 0),
-      squarePos: point(0, 2),
-    },
-    {
-      pos: point(1, 0),
-      pyramidPos: point(1, 1),
-      squarePos: point(2, 0),
-    },
-    {
-      pos: point(2, 2),
-      pyramidPos: point(3, 2),
-      squarePos: point(1, 2),
-    },
-    {
-      pos: point(3, 1),
-      pyramidPos: point(3, 0),
-      squarePos: point(2, 1),
-    },
-  ],
-  [
-    {
-      pos: point(0, 1),
-      pyramidPos: point(0, 0),
-      squarePos: point(0, 2),
-    },
-    {
-      pos: point(1, 0),
-      pyramidPos: point(1, 1),
-      squarePos: point(2, 0),
-    },
-    {
-      pos: point(2, 2),
-      pyramidPos: point(3, 2),
-      squarePos: point(1, 2),
-    },
-    {
-      pos: point(3, 1),
-      pyramidPos: point(2, 1),
-      squarePos: point(3, 0),
-    },
-  ],
-  [
-    {
-      pos: point(0, 1),
-      pyramidPos: point(0, 0),
-      squarePos: point(0, 2),
-    },
-    {
-      pos: point(1, 0),
-      pyramidPos: point(1, 1),
-      squarePos: point(2, 0),
-    },
-    {
-      pos: point(2, 2),
-      pyramidPos: point(1, 2),
-      squarePos: point(3, 2),
-    },
-    {
-      pos: point(3, 1),
-      pyramidPos: point(2, 1),
-      squarePos: point(3, 0),
-    },
-  ],
-  [
-    {
-      pos: point(0, 1),
-      pyramidPos: point(0, 2),
-      squarePos: point(0, 0),
-    },
-    {
-      pos: point(1, 0),
-      pyramidPos: point(1, 1),
-      squarePos: point(2, 0),
-    },
-    {
-      pos: point(2, 2),
-      pyramidPos: point(3, 2),
-      squarePos: point(1, 2),
-    },
-    {
-      pos: point(3, 1),
-      pyramidPos: point(3, 0),
-      squarePos: point(2, 1),
     },
   ],
   [
