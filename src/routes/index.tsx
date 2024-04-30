5d8e88503c9605fecd04a9f54e5231926b0ac170
Jess Law <32jezza@gmail.com>
2024-05-01 00:20:20 +0100

    Add report link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,5 +2,26 @@ import { createFileRoute } from "@tanstack/react-router";
 import { SelectMechanic } from "../SelectMechanic";
 
 export const Route = createFileRoute("/")({
-  component: SelectMechanic,
+  component: () => {
+    return (
+      <>
+        <SelectMechanic />
+        <hr></hr>
+        <footer>
+          Report an issue:
+          <ul>
+            <li>
+              <a
+                href="https://github.com/PeterGerrard/FFXIV-PracticeApp/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title="
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>DM Jezza32 on Discord</li>
+          </ul>
+        </footer>
+      </>
+    );
+  },
 });
